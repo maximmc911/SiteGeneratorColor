62565da5aff918ddb8f1b50ec6ef426758a9fc5c
max <89175221282@mail.ru>
2025-02-23 10:55:30 +0300

    make content in page

--- src/pages/gradientColor/GradientPage.tsx
+++ src/pages/gradientColor/GradientPage.tsx
@@ -1,6 +1,15 @@
+import { useTranslation } from "react-i18next"
+import ContentSection from "./ContentSection"
+
 const GradientPage = () => {
+        const {t} = useTranslation()
   return (
-    <div>GradientPage</div>
+    <>
+       <div className="flex flex-col gap-3 m-2">
+       <h1 className="text-2xl text-center">{t(`Генератор линейных и радиальных градиентов`)}</h1>
+      <ContentSection/> 
+      </div>
+    </>
   )
 }
 
